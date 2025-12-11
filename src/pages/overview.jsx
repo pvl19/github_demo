aa6c6fa62a4d8656682b1acc34016dbf252fde63
Phil Van-Lane <philv19@gmail.com>
2025-12-11 00:43:45 -0800

    added slack and mailing list

--- src/pages/overview.jsx
+++ src/pages/overview.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import overview_text from "../../text/overview.jsx";
+import "./overview.css";
 
 export default function Overview() {
   return (
-    <div>
+    <div className="overview">
       <h1>Overview</h1>
-      <p>{overview_text}</p>
+      <p>SMASH is a collaborative initiative between the departments of Astronomy/Astrophysics and Physics, and the Data Science, AI, and Supercomputer Centers on the UCSD campus. It was started to help researchers at UCSD initiate novel interdisciplinary research collaborations, and strengthen their skills, knowledge, and experience in AI, Machine Learning, and Data Science.
+      <br></br>
+      <br></br>
+      The goal of this initiative is to provide the tools and environment to <b><i>SMASH</i></b> records in the Big Data era of Astronomy & Physics!</p>
+      <h2>Join the SMASH Community!
+      </h2>
+      <p>You can learn more and participate in SMASH activities by:
+      <ul>
+        <li>Joining our <a href="https://join.slack.com/t/ucsd-smash/shared_invite/zt-3kxa3ekbo-GuLL0ZJ_4ZemB_dAKdLtVg" target='_blank' rel='noopener noreferrer'>Slack workspace</a></li>
+        <li>Subscribing to the <a href="https://groups.google.com/g/smash-list/" target="_blank" rel="noopener noreferrer">email list</a></li>
+        </ul>
+      </p>
     </div>
   )
 }
